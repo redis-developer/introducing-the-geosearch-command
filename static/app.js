6e51ede13ab9db085ce59568feeeee054afeca7c
Simon Prickett <simon@redislabs.com>
2021-05-11 13:33:08 -0700

    Limits map interactions.

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  var mymap = L.map('mapid').setView([37.6570598, -122.2636107], 10);
+  var mymap = L.map('mapid', {zoomControl: false, doubleClickZoom: false, dragging: false}).setView([37.6570598, -122.2636107], 10);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
