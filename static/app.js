59efdf85f8fbad9fa5eca967f5109891df19702e
Simon Prickett <simon@redislabs.com>
2021-05-11 16:20:18 -0700

    Adds box controls and toggle behavior.

--- static/app.js
+++ static/app.js
@@ -18,4 +18,34 @@ window.onload = function () {
     document.getElementById('latitude').value = lat;
     document.getElementById('longitude').value = lng;
   });
+
+  document.getElementById('searchType').onchange = function (e) {
+    const radiusElem = document.getElementById('radius');
+    const heightElem = document.getElementById('height');
+    const widthElem = document.getElementById('width');
+
+    if (this.value === 'Radius') {
+      radiusElem.readOnly = false;
+      heightElem.readOnly = true;
+      widthElem.readOnly = true;
+    } else {
+      // Box
+      radiusElem.readOnly = true;
+      heightElem.readOnly = false;
+      widthElem.readOnly = false;
+    }
+  };
+
+  document.getElementById('searchBtn').onclick = function (e) {
+    e.preventDefault();
+    const lat = document.getElementById('latitude').value;
+    const lng = document.getElementById('longitude').value;
+    const radius = document.getElementById('radius').value;
+
+    if ('' !== lat && '' !== lng & '' !== radius) {
+      // TODO call the backend!!
+    } else {
+      alert('Please complete the form!');
+    }
+  };
 };
