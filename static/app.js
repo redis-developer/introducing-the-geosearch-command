c596764baf32a68034c4d6c4f184be98f5a11733
Simon Prickett <simon@redislabs.com>
2021-05-11 15:24:44 -0700

    More scaffolding for the map.

--- static/app.js
+++ static/app.js
@@ -1,7 +1,11 @@
 window.onload = function () {
-  var mymap = L.map('mapid', {zoomControl: false, doubleClickZoom: false, dragging: false}).setView([37.6570598, -122.2636107], 10);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+  const mymap = L.map('mapid', {zoomControl: false, doubleClickZoom: false, touchZoom: false, scrollWheelZoom: false, dragging: false}).setView([37.6570598, -122.2636107], 10);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mymap);
+
+  mymap.on('click', (e) => {
+    alert("You clicked the map at " + e.latlng);
+  });
 };
