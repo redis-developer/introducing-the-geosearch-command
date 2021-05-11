9c2a91f71bfcb4bf84cbadc67372c79c6b300762
Simon Prickett <simon@redislabs.com>
2021-05-11 12:07:04 -0700

    Added code to perform get box search and tidy up results.

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ redis_client = redis.Redis(decode_responses = True)
 
 app = Flask(__name__)
 
+def transform_geosearch_response(response):
+    return [{"name": item[0], "location": { "latitude": item[1][1], "longitude": item[1][0]}} for item in response]
+
 @app.route("/api/search/byradius/<latitude>/<longitude>/<radius>/<radius_unit>")
 def search_by_radius(latitude, longitude, radius, radius_unit):
     # http://localhost:5000/api/search/byradius/37.7589057/-122.3757349/10/mi
     results = redis_client.execute_command(f"geosearch {STATIONS_KEY} fromlonlat {longitude} {latitude} byradius {radius} {radius_unit} withcoord")
-    response = [{"name": item[0], "location": { "latitude": item[1][1], "longitude": item[1][0]}} for item in results]
-    return jsonify(response)
+    return jsonify(transform_geosearch_response(results))
 
-@app.route("/api/search/byrect/<latitude>/<longitude>/<box_width>/<box_height>/<box_unit>")
-def search_by_rectangle(latitude, longitude, box_width, box_height, box_unit):
-    results = redis_client.execute_command(f"geosearch {STATIONS_KEY} fromlonlat {longitude} {latitude} bybox ")
-    return "TODO"
+@app.route("/api/search/bybox/<latitude>/<longitude>/<box_width>/<box_height>/<box_unit>")
+def search_by_box(latitude, longitude, box_width, box_height, box_unit):
+    # http://localhost:5000/api/search/bybox/37.7589057/-122.3757349/5/10/mi
+    results = redis_client.execute_command(f"geosearch {STATIONS_KEY} fromlonlat {longitude} {latitude} bybox {box_width} {box_height} {box_unit} withcoord")
+    return jsonify(transform_geosearch_response(results))
 
 @app.route("/")
 def homepage():
