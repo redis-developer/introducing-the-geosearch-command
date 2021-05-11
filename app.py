ed9caa1aee3bcce8fb71f12d75cae2f01e781f39
Simon Prickett <simon@redislabs.com>
2021-05-11 11:56:24 -0700

    Adds working byradius example.

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ app = Flask(__name__)
 
 @app.route("/api/search/byradius/<latitude>/<longitude>/<radius>/<radius_unit>")
 def search_by_radius(latitude, longitude, radius, radius_unit):
-    print(str(latitude))
-    print(str(longitude))
-    print(str(radius))
-    print(str(radius_unit))
-    return "TODO"
+    # http://localhost:5000/api/search/byradius/37.7589057/-122.3757349/10/mi
+    results = redis_client.execute_command(f"geosearch {STATIONS_KEY} fromlonlat {longitude} {latitude} byradius {radius} {radius_unit} withcoord")
+    response = [{"name": item[0], "location": { "latitude": item[1][1], "longitude": item[1][0]}} for item in results]
+    return jsonify(response)
 
 @app.route("/api/search/byrect/<latitude>/<longitude>/<box_width>/<box_height>/<box_unit>")
-def search_by_rectangle():
+def search_by_rectangle(latitude, longitude, box_width, box_height, box_unit):
+    results = redis_client.execute_command(f"geosearch {STATIONS_KEY} fromlonlat {longitude} {latitude} bybox ")
     return "TODO"
 
 @app.route("/")
