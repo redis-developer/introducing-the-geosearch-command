24d28ed974421abf6cfff7e7aa318911fd4a4eb7
Simon Prickett <simon@redislabs.com>
2021-05-11 15:53:53 -0700

    Adds clickable marker and lat/long updates.

--- static/app.js
+++ static/app.js
@@ -1,11 +1,21 @@
 window.onload = function () {
-  const mymap = L.map('mapid', {zoomControl: false, doubleClickZoom: false, touchZoom: false, scrollWheelZoom: false, dragging: false}).setView([37.6570598, -122.2636107], 10);
+  let userMarker;
+
+  const myMap = L.map('mapid', {zoomControl: false, doubleClickZoom: false, touchZoom: false, scrollWheelZoom: false, dragging: false}).setView([37.6570598, -122.2636107], 10);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(mymap);
+  }).addTo(myMap);
 
-  mymap.on('click', (e) => {
-    alert("You clicked the map at " + e.latlng);
+  myMap.on('click', (e) => {
+    if (userMarker) {
+      myMap.removeLayer(userMarker);
+    }
+    userMarker = L.marker(e.latlng).addTo(myMap);
+
+    const { lat, lng } = e.latlng;
+
+    document.getElementById('latitude').value = lat;
+    document.getElementById('longitude').value = lng;
   });
 };
