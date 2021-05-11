839913204fb841e4775e8c08f86fbe43f9b7355f
Simon Prickett <simon@redislabs.com>
2021-05-11 11:09:56 -0700

    Adds params to routes.

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask import jsonify
+import redis
+from fastkml import kml
+
+STATIONS_KEY = "stations"
+
+redis_client = redis.Redis(decode_responses = True)
 
 app = Flask(__name__)
 
-@app.route("/api/search/byradius")
-def search_by_radius():
+@app.route("/api/search/byradius/<latitude>/<longitude>/<radius>/<radius_unit>")
+def search_by_radius(latitude, longitude, radius, radius_unit):
+    print(str(latitude))
+    print(str(longitude))
+    print(str(radius))
+    print(str(radius_unit))
     return "TODO"
 
-@app.route("/api/search/byrect")
+@app.route("/api/search/byrect/<latitude>/<longitude>/<box_width>/<box_height>/<box_unit>")
 def search_by_rectangle():
     return "TODO"
 
